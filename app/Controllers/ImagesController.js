cb82a7c3995b0c78a8b01e46329d83f2c6b6f645
JoeCalvi <joecalvi@me.com>
2023-02-10 14:29:53 -0700

    background image drawing

--- app/Controllers/ImagesController.js
+++ app/Controllers/ImagesController.js
@@ -1,12 +1,19 @@
+import { appState } from "../AppState.js"
 import { imagesService } from "../Services/ImagesService.js"
 import { Pop } from "../Utils/Pop.js"
+
+function _drawBackgroundImage() {
+    console.log('draw background image')
+    document.body.style.backgroundImage = `url(${appState.image.img})`
+}
 
 export class ImagesController {
     constructor() {
         this.getImages()
+        appState.on('image', _drawBackgroundImage)
     }
 
-    async getImages(){
+    async getImages() {
         try {
             await imagesService.getImages()
         } catch (error) {
--- app/Services/ImagesService.js
+++ app/Services/ImagesService.js
@@ -1,9 +1,12 @@
+import { appState } from "../AppState.js"
+import { Image } from "../Models/Image.js"
 import { sandbox_api } from "./AxiosService.js"
 
 class ImagesService {
     async getImages() {
         const res = await sandbox_api.get('/images')
-        console.log('[images res.data]', res.data)
+        appState.image = new Image(res.data)
+        console.log('[appState.image]', appState.image)
     }
 }
 
